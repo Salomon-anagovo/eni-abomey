2ee270540b98317736034453ae3e53cae1a14e4e
Salomon-anagovo <72698773+Salomon-anagovo@users.noreply.github.com>
2025-04-17 12:13:57 +0100

    Add files via upload

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,35 +1,44 @@
-const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
 
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-}, async (email, password, done) => {
-  try {
-    const user = await User.findOne({ email });
-    if (!user) return done(null, false, { message: 'Email incorrect' });
-    
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) return done(null, false, { message: 'Mot de passe incorrect' });
-    
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-}));
+module.exports = function(passport) {
+  // Configuration de la stratégie locale
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email }).select('+password');
+      if (!user) {
+        return done(null, false, { message: 'Email incorrect' });
+      }
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+      const isMatch = await user.comparePassword(password);
+      if (!isMatch) {
+        return done(null, false, { message: 'Mot de passe incorrect' });
+      }
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
+      user.lastLogin = Date.now();
+      await user.save();
 
-module.exports = passport;
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }));
+
+  // Sérialisation
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  // Désérialisation
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id).select('-password');
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+};
