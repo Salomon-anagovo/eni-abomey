fc684bc96aed1b803a3e7ea9b1d1e0f99c484bde
Salomon-anagovo <72698773+Salomon-anagovo@users.noreply.github.com>
2025-04-18 15:38:25 +0100

    Add files via upload

---
+++ src/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'dsyfvqez3',
+  api_key: '457376748291975',
+  api_secret: 'b6MHplyw3UGs9lqlKM-2x0Lklj8'
+});
+
+module.exports = cloudinary;
